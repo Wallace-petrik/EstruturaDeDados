00e8edd1cb67453139bcb1c4990307eab6661c79
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-04-10 11:01:33 -0300

    Struct Versão 1
    Implementação da struct Aluno

--- EstruturaDeDados/StructVersao1/main.c
+++ EstruturaDeDados/StructVersao1/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <locale.h>
 
+struct{
+    char nome[50];
+    int notas[4];
+    float media;
+}typedef Aluno;
+
 int main(){
 
     setlocale(LC_ALL,"");
-    
 
+    Aluno aluno;
+    int soma = 0;
+
+    printf("Digite o nome do aluno: ");
+        scanf("%[^\n]",aluno.nome);
+    for(int i = 0; i < 4; i++){
+        printf("\nDigite a %dº nota: ",i+1);
+            scanf("%d",&aluno.notas[i]);
+        soma += aluno.notas[i];
+    }
+    aluno.media = soma/4.0;
+    printf("\n\nO aluno %s tem média = %.2f\n ",aluno.nome,aluno.media);
 
     return 0;
 }
